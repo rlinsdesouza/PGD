74c6f1f04f3046e6a42484f3efd3d1dd1cfadb9f
rlinsdesouza <rlinsdesouza@gmail.com>
2019-04-06 13:35:40 -0300

    use errorlog

--- config/logging.php
+++ config/logging.php
@@ -40,8 +40,8 @@ return [
         ],
 
         'single' => [
-            'driver' => 'single',
-            'path' => storage_path('logs/laravel.log'),
+            'driver' => 'errorlog',
+            // 'path' => storage_path('logs/laravel.log'),
             'level' => 'debug',
         ],
 
